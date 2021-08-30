a681b88199c48d7d41023a33b0ba64d96d28d987
Tristan K Truttmann <tristan.truttmann@gmail.com>
2021-08-29 23:05:03 -0500

    Add HeaderIntPair class definition

--- rasdata_helper.py
+++ rasdata_helper.py
@@ -1,5 +1,13 @@
 from enum import Enum
 
+START_RAS_LINE = "*RAS_DATA_START\n" # Begins every file.
+END_RAS_LINE = "*RAS_DATA_END\n"
+END_FILE_LINE = "*DSC_DATA_END\n" # Meaning is rather obscure.
+START_HEADER_LINE = "*RAS_HEADER_START\n"
+END_HEADER_LINE = "*RAS_HEADER_END\n"
+START_INT_LINE = "*RAS_INT_START\n" # Begins intensity sections
+END_INT_LINE = "*RAS_INT_END\n"
+
 class ScanTypes(Enum):
     SCAN1D = 1
     SCAN2D = 2
@@ -10,10 +18,6 @@ class CodeNotCompleteException(Exception):
 class UnexpectedLineException(Exception):
     pass
 
-START_RAS_LINE = "*RAS_DATA_START\n" # Begins every file.
-END_RAS_LINE = "*RAS_DATA_END\n"
-END_FILE_LINE = "*DSC_DATA_END\n" # Meaning is rather obscure.
-START_HEADER_LINE = "*RAS_HEADER_START\n"
-END_HEADER_LINE = "*RAS_HEADER_END\n"
-START_INT_LINE = "*RAS_INT_START\n" # Begins intensity sections
-END_INT_LINE = "*RAS_INT_END\n"
+class HeaderIntPair:
+    def __init__(self,header_lines,int_lines):
+        raise CodeNotCompleteException
