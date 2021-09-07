8c2e2a73d299228c5e73dc303084a5fbadb7186f
Tristan K Truttmann <tristan.truttmann@gmail.com>
2021-09-06 20:25:23 -0500

    Remove redundance methods in RasLoader.

--- rasloader.py
+++ rasloader.py
@@ -2,34 +2,6 @@ from rasloader_helper import *
 
 class RasLoader:
     def __init__(self, filename):
-        self.scantype = self.__determine_scantype(filename)
-        if self.scantype == ScanTypes.SCAN1D: self.__parse1Dras(filename)
-        elif self.scantype == ScanTypes.SCAN2D: self.__parse2Dras(filename)
-    
-    def __determine_scantype(self, filename):
-        f = open(filename,'r')
-        for line in f:
-            if line.split(' ')[0] == "*MEAS_3DE_STEP_AXIS_INTERNAL":
-                return(ScanTypes.SCAN2D)
-        return(ScanTypes.SCAN1D)
-    
-    def __parse1Dras(self, filename):
-        f = open(filename,'r')
-        self.__ensure_nextline_equals(f,START_RAS_LINE)
-        self.__verify_line_equals(f,START_HEADER_LINE)
-        header_lines = self.__extract_header_lines(f)
-        self.__verify_line_equals(f,START_INT_LINE)
-        int_lines = self.__extract_int_lines(f)
-        self.__ensure_nextline_equals(f,END_RAS_LINE)
-        self.__ensure_nextline_equals(f,END_FILE_LINE)
-        self.__ensure_nextline_equals(f,'')
-        f.close()
-        header_int_pair = HeaderIntPair(header_lines,int_lines)
-        self.metadata = header_int_pair.get_final_metadata()
-        self.axisdata = header_int_pair.get_final_axisdata()
-        self.intdata  = header_int_pair.get_final_intdata()
-
-    def __parse2Dras(self, filename):
         f = open(filename,'r')
         self.__ensure_nextline_equals(f,START_RAS_LINE)
         while True:
@@ -45,7 +17,7 @@ class RasLoader:
             elif line == END_RAS_LINE:
                 self.__ensure_nextline_equals(f,END_FILE_LINE)
                 self.__ensure_nextline_equals(f,'')
-
+        
     def __extract_header_lines(self, file):
         lines = []
         while True:
