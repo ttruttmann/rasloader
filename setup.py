7bd07d33e1c33674302097981ccc43ba0e9e0371
Tristan K Truttmann <tristan.truttmann@gmail.com>
2021-09-08 22:41:12 -0500

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'rasloader',
+    version = '1.0.0',
+    description = 'Load Rigaku .ras files',
+    author = 'Tristan K Truttmann',
+    author_email = 'tristan.truttmann@gmail.com'
+)
