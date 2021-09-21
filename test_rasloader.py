1f9db691d448e119ea1ab2bc1bad823837cac796
Tristan K Truttmann <tristan.truttmann@gmail.com>
2021-09-20 23:25:16 -0500

    Add new data files necessary to run tests

--- test_rasloader.py
+++ test_rasloader.py
@@ -2,7 +2,7 @@ import unittest
 from numpy import NAN
 from rasloader import *
 
-class TestRasLoader(unittest.TestCase):
+class TestRasLoader(unittest.TestCase): # TODO: Test appending files.
     def test_constructor_1D(self):
         rasloader = RasLoader('example_scan.ras')
         self.assertEqual(rasloader.metadata['FILE_OPERATOR'],'7290_Tristan')
@@ -28,16 +28,16 @@ class TestRasLoader(unittest.TestCase):
         self.assertIsInstance(rasloader.metadata['HW_GONIOMETER_RADIUS-1'],float)
         self.assertIs(rasloader.axisdata.loc['Omega','position'],NAN)
         self.assertEqual(rasloader.axisdata.loc['Omega','resolution'],0.0001)
-        self.assertEqual(rasloader.axisdata.loc['Phi','position'],1.10)
+        self.assertEqual(rasloader.axisdata.loc['Phi','position'],-2.20)
         self.assertEqual(rasloader.axisdata.loc['Phi','resolution'],0.002)
         self.assertEqual(rasloader.axisdata.loc['Omega','state'],'Scan')
         self.assertEqual(len(rasloader.intdata.columns),3)
         self.assertEqual(rasloader.intdata.columns[0],'Omega')
         self.assertEqual(rasloader.intdata.columns[1],'TwoTheta')
         self.assertEqual(rasloader.intdata.columns[2],'I')
-        self.assertEqual(rasloader.intdata['Omega'][0],22.1) 
-        self.assertEqual(rasloader.intdata['TwoTheta'][0],44.20)
-        self.assertEqual(rasloader.intdata['I'][0],0.6716)
+        self.assertEqual(rasloader.intdata['Omega'][0],15.0) 
+        self.assertEqual(rasloader.intdata['TwoTheta'][0],83.5)
+        self.assertEqual(rasloader.intdata['I'][0],231.292)
         
 if __name__ == '__main__':
     unittest.main()
