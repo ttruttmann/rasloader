87995d4dc64ae72e094e2171591335d0d3733408
Tristan K Truttmann <tristan.truttmann@gmail.com>
2021-11-21 15:35:46 -0600

    Change setup.py from distutils to setuptools and restructure packages

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name = 'rasloader',
     version = '1.0.0',
     description = 'Load Rigaku .ras files',
     author = 'Tristan K Truttmann',
-    author_email = 'tristan.truttmann@gmail.com'
+    author_email = 'tristan.truttmann@gmail.com',
+    license = 'MIT',
+    packages=['rasloader'],
+    zip_safe=False,
+    requires=['numpy','pandas']
 )
